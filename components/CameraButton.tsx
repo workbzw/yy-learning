48684a3c2d570b77ed8c930f7e8743db1fe9dc8d
bzw <workbzw@gmail.com>
2025-04-23 02:48:04 +0800

    [add] camera

--- components/CameraButton.tsx
+++ components/CameraButton.tsx
@@ -1,8 +1,50 @@
+import { useRef } from 'react';
 import Image from 'next/image'
+
 export function CameraButton() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const startCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+    } catch (err) {
+      console.error('Error accessing camera:', err);
+    }
+  };
+
+  const takePhoto = () => {
+    if (videoRef.current && canvasRef.current) {
+      const context = canvasRef.current.getContext('2d');
+      if (context) {
+        context.drawImage(
+          videoRef.current,
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+        );
+        // 这里可以获取照片数据
+        const imageData = canvasRef.current.toDataURL('image/png');
+        console.log(imageData); // 或者上传到服务器
+      }
+    }
+  };
+
   return (
-    <button className="fixed bottom-6 left-1/2 -translate-x-1/2 w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center shadow-lg hover:bg-blue-600">
-      <Image src="/icons/camera.png" alt="相机" width={32} height={32} />
-    </button>
-  )
+    <>
+      <video ref={videoRef} autoPlay style={{ display: 'none' }} />
+      <canvas ref={canvasRef} style={{ display: 'none' }} />
+      <button 
+        className="fixed bottom-6 left-1/2 -translate-x-1/2 w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center shadow-lg hover:bg-blue-600"
+        onClick={startCamera}
+        onDoubleClick={takePhoto}
+      >
+        <Image src="/icons/camera.png" alt="相机" width={32} height={32} />
+      </button>
+    </>
+  );
 }
