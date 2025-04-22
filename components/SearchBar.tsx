0c87fb1f49b1d3310a40c8a6e10740591de36b2d
bzw <workbzw@gmail.com>
2025-04-23 07:06:23 +0800

    [fix] search text color

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,7 +5,7 @@ export function SearchBar() {
       <input
         type="text"
         placeholder="搜索..."
-        className="w-full px-4 py-2 rounded-full border border-gray-200 focus:outline-none focus:border-blue-400"
+        className="w-full px-4 py-2 rounded-full border border-gray-200 focus:outline-none focus:border-blue-400 placeholder-gray-400"
       />
       <button className="absolute right-3 top-1/2 -translate-y-1/2">
         <Image src="/icons/search.png" alt="搜索" width={20} height={20} />
