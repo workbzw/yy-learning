baccf1ec08cd9e52327c0e3f0c547c9d20c68d04
bzw <workbzw@gmail.com>
2025-04-26 21:19:32 +0800

    [add] icon title

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ const Header = () => {
       <div className="flex items-center">
         <Image
           src="/logo.png"
-          alt="粤语学习"
+          alt="粤语智识"
           width={32}
           height={32}
         />
